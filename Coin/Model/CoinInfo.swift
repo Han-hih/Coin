cf91ee1b0b73a6649972ad89ed5132e0c7ca15cf
한 <hih458@naver.com>
2024-04-02 16:34:31 +0900

    [Add] 현재가 가져올 구조체 추가

---
+++ Coin/Model/CoinInfo.swift
@@ -0,0 +1,15 @@
+//
+//  CoinInfo.swift
+//  Coin
+//
+//  Created by 황인호 on 4/2/24.
+//
+
+import Foundation
+
+struct CoinInfo: Hashable {
+   var coinName: String
+   var coinCode: String
+   var change: String
+   var price: Double
+}
