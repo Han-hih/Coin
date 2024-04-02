cd6442af8695fc47c91b50f9f482e99b2ace4048
한 <hih458@naver.com>
2024-04-02 16:28:53 +0900

    [Add] 프로젝트 및 gitignore 생성

---
+++ Coin/CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinApp.swift
+//  Coin
+//
+//  Created by 황인호 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
