d59b5b955ad950fc54c46e603d3df533596fd7f8
한 <hih458@naver.com>
2024-04-12 22:01:19 +0900

    [modify] kstTime Date타입으로 바꾸기 위해 container에서 값 변경, identifiable추가

--- Coin/Model/MinuteCandle.swift
+++ Coin/Model/MinuteCandle.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-struct MinuteCandle: Decodable {
+struct MinuteCandle: Decodable, Identifiable {
+   let id: UUID = UUID()
    let market: String
-   let kstTime: String
+   let kstTime: Date
    let openingPrice: Double
    let highPrice: Double
    let lowPrice: Double
@@ -22,5 +23,15 @@ struct MinuteCandle: Decodable {
 	  case highPrice = "high_price"
 	  case lowPrice = "low_price"
 	  case tradePrice = "trade_price"
+   }
+   
+   init(from decoder: any Decoder) throws {
+	  let container = try decoder.container(keyedBy: CodingKeys.self)
+	  self.market = try container.decode(String.self, forKey: .market)
+	  self.kstTime = try container.decode(String.self, forKey: .kstTime).stringToDate()
+	  self.openingPrice = try container.decode(Double.self, forKey: .openingPrice)
+	  self.highPrice = try container.decode(Double.self, forKey: .highPrice)
+	  self.lowPrice = try container.decode(Double.self, forKey: .lowPrice)
+	  self.tradePrice = try container.decode(Double.self, forKey: .tradePrice)
    }
 }
