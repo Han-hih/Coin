c088eeb713dc7da43cd1dc3f5dd015f6cbc39284
한 <hih458@naver.com>
2024-04-14 23:09:08 +0900

    [Add] 좋아요 목록에 들어갈 구조체 생성

--- Coin/Model/LikeModel.swift
+++ Coin/Model/LikeModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct LikeModel: Decodable, Hashable {
+   var name: String
+   var code: String
+   var rate: String
+   var price: String
+   var diff: String
+   var raise: String
+}
