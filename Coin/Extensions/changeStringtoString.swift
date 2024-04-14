bb59f1445f626d56e4d10e8a63b547dc442a25cc
한 <hih458@naver.com>
2024-04-14 23:08:23 +0900

    [Add] string 변환함수 추가

--- Coin/Extensions/changeStringtoString.swift
+++ Coin/Extensions/changeStringtoString.swift
@@ -9,12 +9,22 @@ import Foundation
 
 extension String {
    func formatStringDateToYMDString() -> String {
-		   let formatter = DateFormatter()
-		   formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-		   guard let date = formatter.date(from: self) else {
-			   return self
-		   }
-		   formatter.dateFormat = "yyyy년 MM월 dd일"
-		   return formatter.string(from: date)
-	   }
+	  let formatter = DateFormatter()
+	  formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+	  guard let date = formatter.date(from: self) else {
+		 return self
+	  }
+	  formatter.dateFormat = "yyyy년 MM월 dd일"
+	  return formatter.string(from: date)
+   }
+   
+   func stringToDate() -> Date {
+	   let dateFormatter = DateFormatter()
+	  dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+	  dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+	   
+	  guard let stirngDate = dateFormatter.date(from: self) else { return Date()
+	  }
+	   return stirngDate
+   }
 }
