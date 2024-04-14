a6f360ce249ada181222ca5ccd264ed25d406e16
한 <hih458@naver.com>
2024-04-14 16:07:28 +0900

    [Add] 커스텀 탭뷰 추가

--- Coin/View/ContentView.swift
+++ Coin/View/ContentView.swift
@@ -7,10 +7,79 @@
 
 import SwiftUI
 
+enum BottomTab {
+   case home, like
+}
+
 struct ContentView: View {
-    var body: some View {
-        MarketListView()
-    }
+   @State var currentTab: BottomTab = .home
+   
+   var body: some View {
+	  ZStack {
+		 tabView
+		 
+		 VStack {
+			Spacer()
+			bottomTabs
+		 }
+	  }
+   }
+}
+
+extension ContentView {
+   var tabView: some View {
+	  
+	  TabView(selection: $currentTab) {
+		 MarketListView()
+			.tag(BottomTab.home)
+			.toolbar(.hidden, for: .tabBar)
+			
+		 LikeView()
+			.tag(BottomTab.like)
+	  }
+	  .toolbar(.hidden, for: .tabBar)
+   }
+   
+   var bottomTabs: some View {
+	  HStack {
+		 Spacer()
+		 Button {
+			self.currentTab = .home
+		 } label: {
+			VStack(alignment: .center) {
+			   Image(systemName: "house")
+				  .resizable()
+				  .scaledToFit()
+				  .frame(width: 30)
+			   Text("홈")
+				  .font(.system(size: 11))
+			}
+			   .padding()
+		 }
+		 Spacer()
+		 Button {
+			self.currentTab = .like
+		 } label: {
+			VStack(alignment: .center) {
+			   Image(systemName: "heart")
+				  .resizable()
+				  .scaledToFit()
+				  .frame(width: 30)
+			   Text("좋아요 목록")
+				  .font(.system(size: 11))
+			}
+			   .padding()
+		 }
+		 Spacer()
+	  }
+	  .frame(height: 72)
+	  .background {
+		 RoundedRectangle(cornerRadius: 30)
+			.fill(Color.white)
+			.shadow(color: .black.opacity(0.3), radius: 8, y: 2)
+	  }
+	  .padding(.horizontal)
+   }
 }
 
 #Preview {
