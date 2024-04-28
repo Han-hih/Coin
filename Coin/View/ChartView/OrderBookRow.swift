7fbe0e2619733799dd21aa12b39cf09b94292ee4
한 <hih458@naver.com>
2024-04-29 04:03:38 +0900

    [Add] 호가와 매도/매수값에 필요한 모델 추가

--- Coin/View/ChartView/OrderBookRow.swift
+++ Coin/View/ChartView/OrderBookRow.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct OrderBookRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+   var price: Double
+   var percent: String
+   var quantity: Double
+   var value: Bool
+   
+   var body: some View {
+	  
+	  HStack {
+		 VStack {
+			Text("\(price)")
+			   .font(.system(size: 15))
+			   .fontWeight(.semibold)
+			Text("\(percent)")
+			   .font(.system(size: 13))
+		 }
+		 .padding(.leading)
+		 .frame(width: 120, height: 50)
+		 .foregroundStyle(value ? .blue : .red)
+		 
+		 HStack {
+			ZStack(alignment: .leading) {
+			   
+			   Rectangle()
+				  .frame(width: 100)
+				  .foregroundStyle(.blue)
+			   
+			   
+			   Text("\(quantity)")
+				  .foregroundStyle(.black.opacity(0.6))
+				  .padding(.leading, 5)
+			   
+			   Spacer()
+			}
+			
+			Spacer()
+		 }
+		 .frame(width: 250, height: 50)
+		 .background(.blue.opacity(0.6))
+		 
+		 
+		 Spacer()
+	  }
+   }
 }
 
 #Preview {
-    OrderBookRow()
+   OrderBookRow(price: 945.0, percent: "+2.8%", quantity: 21.0, value: true)
 }
+
