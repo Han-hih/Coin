cb38f0ad4536303f5876e227d85f0ce56c0ef3e2
한 <hih458@naver.com>
2024-04-29 04:07:21 +0900

    [Add] 호가와 매도/매수값에 필요한 모델 추가

--- Coin/Model/Orderbook.swift
+++ Coin/Model/Orderbook.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+/// 호가
+struct Orderbook: Decodable, Identifiable {
+    let id = UUID()
+	let totalAskSize: Double
+	let totalBidSize: Double
+	let orderbookUnit: [OrderbookUnits]
+
+   enum CodingKeys: String, CodingKey {
+	  case totalAskSize = "total_ask_size"
+	  case totalBidSize = "total_bid_size"
+	  case orderbookUnit = "orderbook_units"
+   }
+}
+
+/// 매도/매수
+struct OrderbookUnits: Identifiable, Decodable {
+   let id = UUID()
+	let askPrice: Double
+	let bidPrice: Double
+	let askSize: Double
+	let bidSize: Double
+   
+   enum CodingKeys: String, CodingKey {
+	  case askPrice = "ask_price"
+	  case bidPrice = "bid_price"
+	  case askSize = "ask_size"
+	  case bidSize = "bid_size"
+   }
+}
